403fdf79f6e8b5419eae5cae6114effb599f524b
Gerardo Merida[B <cgemerida@gmail.com>
2019-01-10 18:52:11 -0600

    Modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,15 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
             'discount' => $this->discount,
+            'totalPrice' => round((1 - $this->discount/100) * $this->price, 2),
+            'stock' => $this->stock > 0 ? $this->stock: 'Out of Stock',
+            'rating' => $this->reviews->count() > 0 ? 
+                round($this->reviews->avg('star'), 2): 
+                'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id),
+            ],
         ];
     }
 }
